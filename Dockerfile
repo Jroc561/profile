4579390888eec0f8164f30bce71298d062ab6edf
Josh Carlisle <Jcarlisle88@gmail.com>
2021-02-24 14:21:30 -0600

    updated with dockerimage

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
-FROM python:3
-WORKDIR /project
-ADD . /project
+FROM python:3.7-stretch
+
+WORKDIR /profile
+
+ADD . /profile
+
+# Install the dependencies
 RUN pip install -r requirements.txt
+
+COPY /profile .
+
+RUN make /profile
+
 CMD ["python", "profile/app.py"]
--- profile/app.py
+++ profile/app.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
+@app.route("/")
+def base():
+    """Our about us page."""
+    return render_template('base.html')
 
-    @app.route("/")
-    def base():
-        """Our about us page."""
-        return render_template('base.html')
-
-
-    return app
+app.run(host='0.0.0.0', port=5000)
